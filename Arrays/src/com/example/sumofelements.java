3881d6c79e6f2593160cb7b801fd54c4301926c6
Bharath-sketch <bharath2005snpatty@gmail.com>
2025-07-09 23:10:20 +0530

    Update sumofelements.java

--- Arrays/src/com/example/sumofelements.java
+++ Arrays/src/com/example/sumofelements.java
@@ -15,3 +15,8 @@ public class sumofelements {
 	        System.out.println("Output: " + sum);
 	    }
 	}
+//Write a program to print the sum of the elements of an array following the given below condition. 
+If the array has 6 and 7 in succeeding orders, ignore the numbers between 6 and 7 and consider the other numbers for calculation of sum. 
+Eg1) Array Elements - 10,3,6,1,2,7,9 O/P: 22 
+[i.e 10+3+9] 
+
