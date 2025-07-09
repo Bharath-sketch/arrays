cf0c617bd60eef9eb4d7f1067885db1063944a18
Bharath-sketch <bharath2005snpatty@gmail.com>
2025-07-09 23:03:52 +0530

    Update arraynumber.java

--- Arrays/src/com/example/arraynumber.java
+++ Arrays/src/com/example/arraynumber.java
@@ -16,3 +16,4 @@ public class arraynumber {
         System.out.println(index);
         }
         }
+//Write a program to initialize an integer array with values and check if a given number is present in the array or not
